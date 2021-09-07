0ecd01d15b42a8370497fa3e44abe240a54d79bc
Erinn Psajd <erinn.psajd@gmail.com>
2021-09-07 09:40:26 -0700

    Created Hello World File

---
+++ hello.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h> //sqrt and powers etc., 
+
+// copies and pastes another text file into the program
+
+int main()
+{
+	int x; // declaration
+
+	x = 7; // initialization
+
+	int y = 5; // both together
+
+
+	int z = x+y;
+
+	printf("Hello World!! %d", z);
+
+	return 0;
+
+	// in gnu editor
+	// gcc the name of the text file. (make sure in same directory)
+	// it should create an exe file 
+	// then you can run executable with ./
+	
+}
+
